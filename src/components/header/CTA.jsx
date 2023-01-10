9aea604a381470ac9fba28624f175db69d651d9e
bwire40 <emmanuelbwire80@gmail.com>
2023-01-10 20:05:29 +0300

    new commits

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,15 +1,18 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import CV from '../../assets/CV.pdf'
+import React from "react";
+import CV from "../../assets/emmanuelbwire_cv.pdf";
 
 const CTA = () => {
   return (
-    <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contacts" className='btn btn-primary'>Let's Talk</a>
-
+    <div className="cta">
+      <a href={CV} download className="btn">
+        Download CV
+      </a>
+      <a href="#contacts" className="btn btn-primary">
+        Let's Talk
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
