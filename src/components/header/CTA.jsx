36a6d2e5a3e673777c941f3282c192ecee2921c1
bwire40 <emmanuelbwire80@gmail.com>
2023-10-03 12:54:43 +0300

    New Commits

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import CV from "../../assets/myCv.pdf";
+import CV from "../../assets/resume.pdf";
 
 const CTA = () => {
   return (
