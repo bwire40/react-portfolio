3b16611397ca9cd419f30d0aef08b0a67eeb656e
bwire40 <emmanuelbwire80@gmail.com>
2025-02-04 13:09:42 +0300

    socials

--- src/components/header/Headersocials.jsx
+++ src/components/header/Headersocials.jsx
@@ -1,17 +1,27 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {BsInstagram} from 'react-icons/bs'
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
 
 const Headersocials = () => {
   return (
-    <div className='header--socials'>
-        <a href="https://linkedin.com" target='_blank' rel="noreferrer"><BsLinkedin /></a>
-        <a href="https://github.com" target='_blank' rel="noreferrer"><FaGithub /></a>
-        <a href="https://instagram.com" target='_blank' rel="noreferrer"><BsInstagram /></a>
+    <div className="header--socials">
+      <a
+        href="https://linkedin.com/in/bwire23"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/bwire40" target="_blank" rel="noreferrer">
+        <FaGithub />
+      </a>
+      <a href="https://instagram.com" target="_blank" rel="noreferrer">
+        <BsInstagram />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Headersocials
+export default Headersocials;
